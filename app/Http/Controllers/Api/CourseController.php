d25870c99230d0974f5e96e971eff0f7d7f0f739
Ibrahim Elmaghrabi <87943179+ibrahim-elmaghrabi@users.noreply.github.com>
2023-10-30 21:17:19 +0200

    edits

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 
@@ -8,6 +8,6 @@ class CourseController extends Controller
 {
     public function index()
     {
-        
+
     }
 }
