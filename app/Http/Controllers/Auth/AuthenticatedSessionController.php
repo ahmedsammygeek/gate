8ae09e723e8e37a907f79f26942cfb200cd708ca
Ibrahim Elmaghrabi <87943179+ibrahim-elmaghrabi@users.noreply.github.com>
2023-11-05 23:16:31 +0200

    fix redirections

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
+use App\Providers\RouteServiceProvider;
+use App\Http\Requests\Auth\LoginRequest;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -27,9 +28,19 @@ class AuthenticatedSessionController extends Controller
     {
         $request->authenticate();
 
+        $user = User::where('email', $request->email)->first();
+
+        if ($user->is_banned) {
+            $message = ' الحساب غير مفعل';
+            Auth::logout();
+
+            return redirect()->back()->with('error', $message);
+        }
+
         $request->session()->regenerate();
 
         return redirect()->intended(RouteServiceProvider::HOME);
+
     }
 
     /**
