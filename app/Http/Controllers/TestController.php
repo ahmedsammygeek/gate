252faa9c65bbd3d8ce67c164f20795d805b69330
ahmedsammygeek <ahmedsamigeek@gmail.com>
2023-12-20 18:06:56 +0200

    ...

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use App\Models\University;
+use App\Models\Course;
 class TestController extends Controller
 {
   public function index()
   {
 
+    $courses = Course::has('units')->get();
 
+    foreach ($courses as $course) {
+      $lesson = $course->units->first()->lessons()->first();
+      $lesson->is_free = 1;
+      $lesson->save();
+      dd($lesson->save());
+    }
   }
 }
---
+++ database/migrations/2023_12_20_160018_add_is_free_col_to_lessons_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->tinyInteger('is_free')->default(0)->comment('if 0 it means not free 1 means it is free');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('lessons', function (Blueprint $table) {
+            $table->dropColumn('is_free');
+        });
+    }
+};
