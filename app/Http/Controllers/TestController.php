4baa7fab6eb7419882b8d3b42ec0d45f98bbba11
ahmedsammygeek <ahmedsamigeek@gmail.com>
2023-12-22 21:08:53 +0200

    ...

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,11 +5,21 @@ namespace App\Http\Controllers;
 use Exception;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Spatie\Permission\Models\Permission;
+
 class TestController extends Controller
 {
   public function index()
   {
 
+    $permissions = Permission::all();
+
+    $admin = User::first();
+
+    // $admin-
+
+
+     $admin->syncPermissions($permissions);
 
 
      // Gate::before(function ($user, $ability) {
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -21,8 +21,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function ($user, $ability) {
-            return $user->email == 'admin@thegatelearning.com' ? true : false;
-        });
+
     }
 }
