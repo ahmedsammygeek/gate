8bb29cd24e62c10c539b3d677990e0f8358f1246
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-01-30 18:14:59 +0200

    ...

--- app/Http/Resources/BasicUserResource.php
+++ app/Http/Resources/BasicUserResource.php
@@ -14,7 +14,7 @@ class BasicUserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'image' =>  Storage::url('users/' . $this->image),
+            'image' =>  Storage::url('trainers/' . $this->image),
         ];
     }
 }
