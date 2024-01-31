0ff816a6d356a466fd429dfc623825e2be33e2ac
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-01-31 20:58:18 +0200

    ...

--- app/Http/Controllers/Api/CheckoutController.php
+++ app/Http/Controllers/Api/CheckoutController.php
@@ -23,11 +23,30 @@ class CheckoutController extends Controller
 
     public function index(CheckoutRequest $request)
     {
+
+
         $course = Course::find($request->course_id);
         if (!$course) {
             return response()->json([
-                'status' => true,
-                'message' => 'success',
+                'status' => false,
+                'message' => 'we can not find this course',
+                'data' =>  []
+            ]);
+        }
+
+        if (!$course->is_active) {
+            return response()->json([
+                'status' => false,
+                'message' => 'this course can not be purchased',
+                'data' =>  []
+            ]);
+        }
+
+
+        if ($course->ends_at <= Carbon::today() ) {
+            return response()->json([
+                'status' => false,
+                'message' => 'this course is expired and can not be purchased',
                 'data' =>  []
             ]);
         }
@@ -49,27 +68,27 @@ class CheckoutController extends Controller
             $first_installment = $course->installments()->where('days' , 0 )->first();
             $payment_types['installments']['amount_due_today']  = $first_installment ? $first_installment->amount : 0 ;
             if ($first_installment) {
-               $payment_types['installments']['first_payment_date']  = Carbon::today()->toDateString();
-           } else {
-            $first_installment =  $course->installments()->orderBy('days' , 'ASC' )->first();
-            $payment_types['installments']['first_payment_date']  = Carbon::today()->addDay($first_installment->days)->toDateString();
+                $payment_types['installments']['first_payment_date']  = Carbon::today()->toDateString();
+            } else {
+                $first_installment =  $course->installments()->orderBy('days' , 'ASC' )->first();
+                $payment_types['installments']['first_payment_date']  = Carbon::today()->addDay($first_installment->days)->toDateString();
+            }
+
         }
 
+        $info = Setting::first();
+        return response()->json([
+            'status' => true,
+            'message' => 'success',
+            'data' =>  (object) [
+                'payment_methods' => new PaymentSettingsResource($info) , 
+                'courses_details' => new BasicCourseResource($course) , 
+                'payment_types' => $payment_types , 
+                'can_purchase_this_item' => $this->canPurchaseThisItem($course) , 
+            ] , 
+        ]);
+
     }
-
-    $info = Setting::first();
-    return response()->json([
-        'status' => true,
-        'message' => 'success',
-        'data' =>  (object) [
-            'payment_methods' => new PaymentSettingsResource($info) , 
-            'courses_details' => new BasicCourseResource($course) , 
-            'payment_types' => $payment_types , 
-            'can_purchase_this_item' => $this->canPurchaseThisItem($course) , 
-        ] , 
-    ]);
-
-}
 
     /**
      * Show the form for creating a new resource.
