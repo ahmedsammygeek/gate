ce23370870ed24a1a1342cafda3685b9ad962b11
ahmedsammygeek <ahmedsamigeek@gmail.com>
2024-02-02 22:16:50 +0200

    ...

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -28,8 +28,8 @@ class ProfileResource extends JsonResource
             'updated_ar' => $this->updated_at , 
             'activated_at' => $this->activated_at , 
             'unread_notifications_count' => $this->unreadNotifications->count() , 
-            'courses_count' => $this->courses()->where( 'related_package_id' , null)->count() , 
-            'eligible_installments' => $this->installments()->whereDate('due_date' , '>=' , Carbon::today() )->count() , 
+            'courses_count' => $this->courses()->where('expires_at' , '>' , Carbon::today() )->where( 'related_package_id' , null)->count() , 
+            'eligible_installments' => $this->installments()->where('status' , 0 )->whereDate('due_date' , '>=' , Carbon::today() )->count() , 
             'course_progress' => UserCourseProgressResource::collection(Auth::user()->courses) , 
             'can_change_whats_number' => $this->canUserChangeWhatsAppNumber() , 
         ];
